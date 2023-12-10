808a2bd4920493430421393a22caa8f8db38e9db
tehuberjohn <tehuberjohn@hotmail.co.uk>
2023-12-10 12:48:34 -0600

    added day 5 part one, started setting up for part 2

--- 2023/Day-5/solution.js
+++ 2023/Day-5/solution.js
@@ -1,48 +1,79 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("sample.txt").toString().split("\r\n");
+const data = fs.readFileSync("input.txt").toString().split("\r\n");
 // console.log(data);
 
-const formatNums = (str) => str.split(" ").map(Number);
-
-const formatMapData = (arr) => {
-  const name = arr[0].split(" ")[0];
-  const [from, to] = name.split("-to-");
-  const values = [];
-  for (const line of arr.slice(1)) {
-    const [destinationRangeStart, sourceRangeStart, rangeLength] =
-      formatNums(line);
-    values.push({
-      destinationRangeStart,
-      sourceRangeStart,
-      rangeLength,
-    });
+class Almanac {
+  constructor(data) {
+    this.rawData = this.parseData(data);
+    this.seeds = this.formatNumArr(this.rawData[0]);
+    this.pages = this.rawData[1].map(this.formatPageData);
   }
-  return {
-    from,
-    to,
-    values,
+  parseData = () => {
+    const sets = [];
+    let current = [];
+    for (const line of data) {
+      if (!line) {
+        sets.push(current);
+        current = [];
+      } else {
+        current.push(line);
+      }
+    }
+    sets.push(current);
+    const seeds = sets[0][0].split(":")[1].trim();
+    const pages = sets.slice(1);
+    return [seeds, pages];
   };
-};
+  formatPageData = (arr) => {
+    const name = arr[0].split(" ")[0];
+    const [from, to] = name.split("-to-");
+    const values = [];
+    for (const line of arr.slice(1)) {
+      const [destinationRangeStart, sourceRangeStart, rangeLength] =
+        this.formatNumArr(line);
+      values.push({
+        destinationRangeStart,
+        sourceRangeStart,
+        rangeLength,
+      });
+    }
+    return {
+      from,
+      to,
+      values,
+    };
+  };
+  formatNumArr = (str) => {
+    return str.split(" ").map(Number);
+  };
+}
 
-const parseData = () => {
-  const sets = [];
-  let current = [];
-  for (const line of data) {
-    if (!line) {
-      sets.push(current);
-      current = [];
-    } else {
-      current.push(line);
+const lookupSeedLocation = (num, values) => {
+  for (const value of values) {
+    const min = value.sourceRangeStart;
+    const max = value.sourceRangeStart + value.rangeLength;
+    if (num >= min && num <= max) {
+      const offset = num - min;
+      return value.destinationRangeStart + offset;
     }
   }
-  sets.push(current);
-  const seeds = sets[0][0].split(":")[1].trim();
-  const maps = sets.slice(1);
-  return [formatNums(seeds), maps.map(formatMapData)];
+  return num;
 };
 
-let [seeds, maps] = parseData();
-console.log([maps[0], maps[0].values]);
-const solve = () => {};
-//parsed data and tried afew tests to try and understand the problem, will work on figuring out what im even doing later XD
+const getSeeds = (arr) => {};
+
+const almanac = new Almanac(data);
+const solve = (book, part) => {
+  const seeds = part === "one" ? book.seeds : getSeeds(book.seeds);
+  const locations = [];
+  for (const seed of seeds) {
+    let location = seed;
+    for (const page of book.pages) {
+      location = lookupSeedLocation(location, page.values);
+    }
+    locations.push(location);
+  }
+  console.log(`Part ${part}: ${Math.min(...locations)}`);
+};
+solve(almanac, "one");
